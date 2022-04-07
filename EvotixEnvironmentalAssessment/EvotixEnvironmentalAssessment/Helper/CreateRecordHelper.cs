02337a836a1ea626dcce6a189ad4ef7ac2578be1
paulb1986 <103224728+paulb1986@users.noreply.github.com>
2022-04-08 00:34:50 +0100

    feat: add project

---
+++ EvotixEnvironmentalAssessment/EvotixEnvironmentalAssessment/Helper/CreateRecordHelper.cs
@@ -0,0 +1,15 @@
+﻿using EvotixEnvironmentalAssessment.Models;
+using EvotixEnvironmentalAssessment.Pages;
+
+namespace EvotixEnvironmentalAssessment.Helper
+{
+    public class CreateRecordHelper
+    {
+        public static void SetEnvironmentRecordDetailsAndSave(EnvironmentalRecord record)
+        {
+            CreateRecordPage.SetEnvironmentalDetails(CreateRecordPage.DESC_TEXT_AREA, record.description);
+            CreateRecordPage.SetEnvironmentalDetails(CreateRecordPage.DATE_INPUT, record.date);
+            CreateRecordPage.ClickSaveAndCloseButton();
+        }
+    }
+}
